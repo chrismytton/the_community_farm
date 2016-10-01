d5f8ccb28b4db21f7967972eb2354e598b36a598
Chris Mytton <chrismytton@gmail.com>
2016-10-01 10:28:11 +0100

    Switch to describe syntax in tests

--- test/the_community_farm_test.rb
+++ test/the_community_farm_test.rb
@@ -1,11 +1,7 @@
 require 'test_helper'
 
-class TheCommunityFarmTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::TheCommunityFarm::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
+describe TheCommunityFarm do
+  it 'has a version number' do
+    ::TheCommunityFarm::VERSION.wont_be_nil
   end
 end
